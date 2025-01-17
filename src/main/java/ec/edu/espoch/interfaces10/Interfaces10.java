e191b0fe7b295b143de06a769c7327ac60038289
Stalyn0101 <yixon11@gmail.com>
2025-01-17 16:37:26 -0500

    f

--- src/main/java/ec/edu/espoch/interfaces10/Interfaces10.java
+++ src/main/java/ec/edu/espoch/interfaces10/Interfaces10.java
@@ -1,16 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package ec.edu.espoch.interfaces10;
 
-/**
- *
- * @author USER
- */
+
 public class Interfaces10 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello sssss");
     }
 }
